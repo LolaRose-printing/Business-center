660d45d9430afebf930b5c8bef0ff2b90ac740e1
SartorFit Inc <143529618+TKpopoola-consulting@users.noreply.github.com>
2025-07-15 16:48:46 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
-import "../styles/globals.css";  
+
 
 
 import "./globals.css";
