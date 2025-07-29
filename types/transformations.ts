7a9513824cb8a4691dced1cf2970cb944af70041
SartorFit Inc <143529618+TKpopoola-consulting@users.noreply.github.com>
2025-07-29 10:00:19 -0500

    Update transformations.ts

--- types/transformations.ts
+++ types/transformations.ts
@@ -1,5 +1,7 @@
 export type Transformations =
   | { restore: boolean }
   | { removeBackground: boolean }
-  | { fillBackground: boolean } // ✅ Add this line!
+  | { fillBackground: boolean }
+  | { remove: { prompt: string; removeShadow: boolean; multiple: boolean } }  // ✅ Add this!
+  | { recolor: { prompt: string; to: string; multiple: boolean } }  // ✅ And maybe this too!
   | { someOtherOption?: string };
