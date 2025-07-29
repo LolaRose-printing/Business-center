38d7a260efd3ff178e08fe4f2f9b08651edbe357
SartorFit Inc <143529618+TKpopoola-consulting@users.noreply.github.com>
2025-07-29 12:10:57 -0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
         name: "business-center",
         script: "node_modules/next/dist/bin/next",
-        args: "start -p 3000",
+        args: "start -p 3004",
         env: {
           NODE_ENV: "production",
         },
